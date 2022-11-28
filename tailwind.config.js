6f8100b28bf92db1f125493d5553ba24942fb982
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2022-11-28 17:25:11 +0000

    installed tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  module: 'jit',
+  darkMode: false, // 'media' or 'class'
+  purge: {
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      // https://purgecss.com/safelisting.html#patterns
+      safelist: {
+        standard: [/^bg-/, /^text-/],
+      },
+    },
+  },
   content: [],
   theme: {
     extend: {},
