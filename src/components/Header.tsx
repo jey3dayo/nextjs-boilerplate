7b94756a06d0ec8410f3ae342e0a935e38e9a166
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2022-12-07 09:05:00 +0000

    headerにstate呼び出し処理追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,16 @@
+import { useRecoilState } from 'recoil';
+import { userState } from '@/atoms/userAtom';
 import { appName } from '@/config';
 
 export default function Header() {
-  return <div className={'bg-primary p-2 text-secondary'}>{appName}</div>;
+  const [user] = useRecoilState(userState);
+  return (
+    <>
+      <div className={'flex'}>
+        <div className="flex-none bg-primary p-2 text-secondary">{appName}</div>
+        <div className="flex-1 bg-secondary p-2 text-secondary">{appName}</div>
+        <div className={'flex-none bg-primary p-2 text-right text-secondary'}>wlecome {user?.name ?? ''}.</div>
+      </div>
+    </>
+  );
 }
